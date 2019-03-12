b98c7f119d23ae4244b6e6e06b9844c16c2b75f2
popescualexandru9 <popscu_alexandru9@yahoo.com>
2019-03-12 19:21:35 +0200

    Update AngajatHeader.cpp

--- Tema2/AngajatHeader.cpp
+++ Tema2/AngajatHeader.cpp
@@ -10,25 +10,25 @@ Angajat::~Angajat()
 
 Angajat::Angajat()
 {
-	nume = new char[5];
+    nume = new char[5];
     strcpy_s(nume,5,"Nume");
     salariu=0.0;
 }
 
 Angajat::Angajat(char *n,float sal)
 {
-	int lenght = strlen(n);
-	nume = new char[lenght+1];
-	strcpy_s(nume,lenght+1, n);
+    int lenght = strlen(n);
+    nume = new char[lenght+1];
+    strcpy_s(nume,lenght+1, n);
     salariu=sal;
 }
 
 Angajat::Angajat(const Angajat &r)
 {
-	int lenght = strlen(r.nume);
-	nume = new char[lenght + 1];
-	strcpy_s(nume,lenght+1 ,r.nume);
-    this->salariu=r.salariu;
+     int lenght = strlen(r.nume);
+     nume = new char[lenght + 1];
+     strcpy_s(nume,lenght+1 ,r.nume);
+     this->salariu=r.salariu;
 }
 
 void Angajat::display()
